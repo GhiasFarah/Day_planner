9c1d964f22c4541ed4a6db605ae6332dcecc452d
farah <farah9hias@gmail.com>
2022-05-06 17:45:33 -0700

    reformatted javascript

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
 var H = moment().format('H')
 var timeBlock = $('.timeblock')
-for(let i = 0; i < localStorage.length; i++){
-    $('.'+localStorage.key(i).trim()).siblings('textarea').val(localStorage.getItem(localStorage.key(i)))
+for (let i = 0; i < localStorage.length; i++) {
+    $('.' + localStorage.key(i).trim()).siblings('textarea').val(localStorage.getItem(localStorage.key(i)))
 }
 
 $('#currentDay').text(moment().format('MMMM Do YYYY'))
 var saveBtn = $('.saveBtn')
 
-$('.saveBtn').click(function(e){
-e.preventDefault()
-localStorage.setItem($(this).siblings('p').text(), $(this).siblings('textarea').val())
+$('.saveBtn').click(function (e) {
+    e.preventDefault()
+    localStorage.setItem($(this).siblings('p').text(), $(this).siblings('textarea').val())
 })
 
-$(document).ready(function(){
+$(document).ready(function () {
     for (i = 9; i <= 17; i++) {
-      var timeBlock = parseInt($('#'+i).attr('id'))
-      var currentTime = parseInt(moment().format('H'));
-    
-  if (timeBlock < currentTime) {
-    $('#'+i).siblings('textarea').addClass('past')
-  }
-  else if (timeBlock > currentTime) {
-    $('#'+i).siblings('textarea').addClass('future')
-  }
-  else if (timeBlock = currentTime) {
-    $('#'+i).siblings('textarea').addClass('present')
-  }
-  }
-  });
+        var timeBlock = parseInt($('#' + i).attr('id'))
+        var currentTime = parseInt(moment().format('H'));
+
+        if (timeBlock < currentTime) {
+            $('#' + i).siblings('textarea').addClass('past')
+        }
+        else if (timeBlock > currentTime) {
+            $('#' + i).siblings('textarea').addClass('future')
+        }
+        else if (timeBlock = currentTime) {
+            $('#' + i).siblings('textarea').addClass('present')
+        }
+    }
+});
